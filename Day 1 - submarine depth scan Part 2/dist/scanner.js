de19019dbca8da69ab1b74df117775185b64e6d4
Alexander Horner <mail@alexanderhorner.com>
2021-12-02 00:11:08 +0100

    Simplfiy

--- Day 1 - submarine depth scan Part 2/dist/scanner.js
+++ Day 1 - submarine depth scan Part 2/dist/scanner.js
@@ -3,15 +3,9 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const data_1 = require("./data");
 var count = 0;
 for (let i = 3; i < data_1.data.length; i++) {
-    // We can simplyify following approach by ignoring data[i-2] and data[i-1], since they get added on both:
-    // var currentWindow = data[i] + data [i-1] + data[i-2]
-    // var previousWindow = data[i-1] + data[i-2] +data[i-3]
-    var currentWindow = data_1.data[i];
-    var previousWindow = data_1.data[i - 3];
-    if (currentWindow > previousWindow) {
+    if (data_1.data[i] > data_1.data[i - 3]) {
         count++;
     }
 }
 console.log(count);
-// 1457
 //# sourceMappingURL=scanner.js.map
--- Day 1 - submarine depth scan Part 2/src/scanner.ts
+++ Day 1 - submarine depth scan Part 2/src/scanner.ts
@@ -1,22 +1,11 @@
 import { data } from './data'
 
-
 var count: number = 0
 
 for (let i = 3; i < data.length; i++) {
-
-    // We can simplyify following approach by ignoring data[i-2] and data[i-1], since they get added on both:
-    // var currentWindow = data[i] + data [i-1] + data[i-2]
-    // var previousWindow = data[i-1] + data[i-2] +data[i-3]
-
-    var currentWindow = data[i]
-    var previousWindow = data[i-3]
-
-    if (currentWindow > previousWindow) {
+    if (data[i] > data[i-3]) {
         count++
     }
 }
 
 console.log(count)
-
-// 1457
